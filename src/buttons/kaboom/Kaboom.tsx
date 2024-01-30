53fc3a3ed2784059813b802c0e5fbf83396f567f
ivan k <ivan@sq10.net>
2024-01-30 15:46:51 -0800

    kaboom media

---
+++ src/buttons/kaboom/Kaboom.tsx
@@ -0,0 +1,20 @@
+// import { onMount, type Component } from "solid-js";
+import kaboomWebmUrl from "./kaboom.webm";
+
+// const kaboomAudio = new Audio(kaboomSndUrl);
+
+// const kaboomVd = document.createElement("video");
+// kaboomVd.src = kaboomAnimUrl;
+
+// const MeowButton: Component = () => {
+//   let button: HTMLButtonElement;
+//   onMount(() => {
+//     button.addEventListener("mousedown", () => {});
+//   });
+
+//   return (
+//     <button ref={button} class="p-4 m-4">
+//       🧨
+//     </button>
+//   );
+// };
